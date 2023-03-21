d6d1948e377e981413dc01ee5ad249ee2dc51ab0
Cristian <cristian@Cristians-MacBook-Pro.local>
2023-03-21 16:56:54 -0500

    MUI + Styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@mui/material'
+import styled from '@emotion/styled'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +24,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Title>Vite + React + MUI + Styled components</Title>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,6 +32,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <Button>MUI Button</Button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
